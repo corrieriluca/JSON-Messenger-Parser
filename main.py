f2acf99b570e732d24e10ae35cb77f11f48ced67
Luca Corrieri <luca.corrieri@epita.fr>
2019-03-30 15:02:40 +0100

    Basic tests and learning with JSON

--- main.py
+++ main.py
@@ -3,15 +3,33 @@
 
 import datetime
 import json
-from enum import Enum
-from DateFormatter import dateFormat, frenchDateFormat
+from dateFormatter import dateFormat, frenchDateFormat
 
-s = 1553939328055
-content = "some message"
+class Message():
+    def __init__(self, sender, timestamp, content, date):
+        self.sender = sender
+        self.timestamp = timestamp # timestamp for sorting messages
+        self.content = content
+        self.date = date # readable date from timestamp
+
+    def displayMessage(self):
+        display = self.sender + ' a écrit: ' + self.content + ' le ' + self.date
+        return display
+
+# JSON file
+with open("test.json") as file:
+    data = file.read()
+
+jsonConv = json.loads(data)
+
+messageNumber = len(jsonConv["messages"])
+
+firstMessageJSON = jsonConv["messages"][messageNumber - 1]
+firstMessage = Message(firstMessageJSON["sender_name"], firstMessageJSON["timestamp_ms"], firstMessageJSON["content"], frenchDateFormat(firstMessageJSON["timestamp_ms"]))
+
 
 # Uncomment the following line and comment the line for french format to use english format of date
 # readableDate = datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S.%f') # english format
 # readableDate = datetime.datetime.fromtimestamp(s).strftime('%d-%m-%Y %H:%M:%S') # french format
 
-print(dateFormat(s))
-print(frenchDateFormat(s))
+print(firstMessage.displayMessage())
